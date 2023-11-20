6d7a72d1e437c14adfc180f1d725e0319f3a149e
Oleksandr Tymoshenko <oleksandr.tymoshenko@engageli.com>
2023-11-20 21:48:08 +0100

    inject loggable

--- mediasoup-client/src/main/java/org/mediasoup/droid/MediasoupClient.java
+++ mediasoup-client/src/main/java/org/mediasoup/droid/MediasoupClient.java
@@ -2,32 +2,35 @@ package org.mediasoup.droid;
 
 import android.content.Context;
 
+import org.webrtc.Loggable;
+import org.webrtc.Logging;
 import org.webrtc.PeerConnectionFactory;
 import org.webrtc.PeerConnectionFactory.InitializationOptions;
 
 public class MediasoupClient {
 
-  static {
-    System.loadLibrary("mediasoupclient_so");
-  }
+    static {
+        System.loadLibrary("mediasoupclient_so");
+    }
 
-  public static void initialize(Context appContext) {
-    initialize(appContext, null);
-  }
+    public static void initialize(Context appContext) {
+        initialize(appContext, null, null, null);
+    }
 
-  public static void initialize(Context appContext, String fieldTrials) {
-    InitializationOptions options =
-        InitializationOptions.builder(appContext)
-            .setFieldTrials(fieldTrials)
-            .setEnableInternalTracer(true)
-            .setNativeLibraryName("mediasoupclient_so")
-            .createInitializationOptions();
-    PeerConnectionFactory.initialize(options);
-  }
+    public static void initialize(Context appContext, String fieldTrials, Loggable loggable, Logging.Severity severity) {
+        InitializationOptions options =
+                InitializationOptions.builder(appContext)
+                        .setFieldTrials(fieldTrials)
+                        .setEnableInternalTracer(true)
+                        .setNativeLibraryName("mediasoupclient_so")
+                        .setInjectableLogger(loggable, severity)
+                        .createInitializationOptions();
+        PeerConnectionFactory.initialize(options);
+    }
 
-  public static String version() {
-    return nativeVersion();
-  }
+    public static String version() {
+        return nativeVersion();
+    }
 
-  private static native String nativeVersion();
+    private static native String nativeVersion();
 }
